8a8c1bf36da7c2f849427c41d344a672f8b4f6d5
cartisan <leon@die-genossen.de>
2014-05-17 04:49:36 +0200

    first part of c-value calculation

--- knowext/c-nc.py
+++ knowext/c-nc.py
@@ -4,15 +4,16 @@ import nltk
 from nltk.tag.simplify import simplify_wsj_tag
 from nltk.util import ngrams
 
-multi_term_parser = nltk.RegexpParser('CHUNK: {<N|ADJ>*<N>}')
-
-
-def c_value(token, max_len):
-    pass
+MULTI_TERM_PARSER = nltk.RegexpParser('CHUNK: {<N|ADJ>*<N>}')
 
 
 def find_multi_word_terms(pos_tags):
-    # find all multi word terms
+    def _candidate_words(pos_tags):
+        tree = MULTI_TERM_PARSER.parse(pos_tags)
+        return [subtree.leaves() for subtree in tree.subtrees()
+                if subtree.node == "CHUNK"]
+
+    # find all maximal multi word terms
     candidates = _candidate_words(pos_tags)
 
     # check weather candidates contain sub words
@@ -27,10 +28,13 @@ def find_multi_word_terms(pos_tags):
     return candidates+sub_words
 
 
-def _candidate_words(pos_tags):
-    tree = multi_term_parser.parse(pos_tags)
-    return [subtree.leaves() for subtree in tree.subtrees()
-            if subtree.node == "CHUNK"]
+def c_value(ngram, corpus, max_n):
+    if len(ngram) == max_n:
+        # zip(*ngram)[0] returns a tuple with words from a (word,tag) list
+        return log(max_n, 2) * text.lower().count(" ".join(zip(*ngram)[0]))
+
+    # TODO: fill in!
+    return 0
 
 
 # download nltk resources for tokenization
@@ -56,6 +60,7 @@ candidates = find_multi_word_terms(pos_tags)
 
 # c-vaues
 max_len = max([len(ngram) for ngram in candidates])
-c_values = [log(len(ngram), 2) for ngram in candidates]
+c_values = [c_value(ngram, text, max_len) for ngram in candidates]
 
 # nc-values
+pass
