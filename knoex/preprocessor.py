bb455f7c2e20939cd66a12c4acf8f132e9e11957
mome <npsjua@gmail.com>
2014-06-26 13:34:38 +0200

    added method run different parsers (i.e. stanford)

--- knoex/preprocessor.py
+++ knoex/preprocessor.py
@@ -1,9 +1,11 @@
 from utils import setup_nltk_resources
 
-from nltk import word_tokenize
+from nltk import word_tokenize, Tree
 from nltk import pos_tag as nltk_pos_tag
 from nltk.tag.simplify import simplify_wsj_tag
 from nltk.tokenize import sent_tokenize
+from stat_parser import parser as s_parser
+import os
 
 
 def pos_tag(text, simple=False):
@@ -43,3 +45,51 @@ def pos_tag(text, simple=False):
         pos = simple_pos
 
     return pos
+
+
+def parse_sentence(sentence, parser='stanford', path_to_parser=None):
+
+    if parser == 'stanford':
+
+        if path_to_parser == None :
+            path_to_parser = './stanford-parser-full-2014-01-04'
+        elif path_to_parser[-1] == '/' :
+            path_to_parser = path_to_parser[:-1]
+
+
+        # saves the sentence in a temporary file
+        os.popen("echo '" + sentence + "' > ~/stanfordtemp")
+
+        # calles the stanford parser and outputs string representation of parse tree
+        parser_out = os.popen(path_to_parser + "/lexparser.sh ~/stanfordtemp").readlines()
+        #os.remove('~/stanfordtemp')
+
+        # transform the stanford parse tree representation into nltk parse tree representation
+        parse_trees_text=[]
+        tree = ""
+        for line in parser_out:
+            if line.isspace():
+                parse_trees_text.append(tree)
+                tree = ""
+            elif "(. ...))" in line:
+                #print "YES"
+                tree = tree + ')'
+                parse_trees_text.append(tree)
+                tree = ""
+            else:
+                tree = tree + line
+
+        # neglect parse_trees_text[1] at this point
+        print tree
+        parse_tree = Tree(parse_trees_text[0])
+
+
+    elif parser in ['s_parser','stat','stat_parser']:
+        sp = s_parser.Parser()
+        parse_tree = sp.parse(sentence)
+
+    else :
+        print 'No such parser:', parser
+        parse_tree = None
+
+    return parse_tree   
