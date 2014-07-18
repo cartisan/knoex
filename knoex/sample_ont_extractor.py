a71fcff2cb557b3feb1c8ebc8f18e575d8593ede
cartisan <leon@die-genossen.de>
2014-07-18 04:00:04 +0200

    tested on snake corpus again, seems to work

--- knoex/sample_ont_extractor.py
+++ knoex/sample_ont_extractor.py
@@ -2,20 +2,16 @@ from corpus import CorpusReader
 from c_nc import C_NC_TermExtractor
 from concept_former import conceptFormer as ConceptFormer
 import hearst_patterns as RelationExtractor
-from term import Term
+from pprint import pprint
 
-c = CorpusReader("knoex/corpora/snakes.corp")
+c = CorpusReader("corpora/snakes.corp")
 text = c.get_corpus()
 
-#term_extractor = C_NC_TermExtractor(text)
-#terms = term_extractor.compute_cnc()
+term_extractor = C_NC_TermExtractor(text)
+terms = term_extractor.compute_cnc()
 
-#terms = [#Term([("snake", "N")]),
-         #Term([("snake", "N")]),
-terms = [Term([("cake", "N")]),
-         Term([("green", "ADJ"), ("snake", "N")])]
 taxonomy = ConceptFormer().form_concepts(terms)
-print taxonomy
+pprint(taxonomy)
 
 #relations = RelationExtractor.find_realation(text)
 #print relations
